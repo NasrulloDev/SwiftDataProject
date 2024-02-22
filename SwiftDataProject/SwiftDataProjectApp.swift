dffa0aec5cd92964356e0a0bfecbc092bb1f23cb
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-22 18:29:34 +0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Насрулло Исмоилжонов on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
